84b2968c2716156310238664661f906c11063b9a
s216100801 <slabiti181@gmail.com>
2018-05-12 03:46:53 +0200

    operations

---
+++ app/src/main/java/com/example/s216100801/homedashboard/Home.java
@@ -0,0 +1,13 @@
+package com.example.s216100801.homedashboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
