baaa7159d8d60c2e5ea4276d886b5cc1d754fcac
s216100801 <slabiti181@gmail.com>
2018-06-05 15:09:54 +0200

    Request gui done

--- app/src/main/java/com/divide/ibitech/divide_ibitech/Request.java
+++ app/src/main/java/com/divide/ibitech/divide_ibitech/Request.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -20,8 +21,10 @@ import android.graphics.Color;
 
 public class Request extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener mDateSetListener;
-
-
+    Boolean valid;
+         String dName,p_Name,date;
+         EditText DName, PName;
+         TextView Date ,tRequest;
     private static final String TAG = "MainActivity";
 
     private TextView mDisplayDate;
@@ -31,6 +34,46 @@ public class Request extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
         mDisplayDate = (TextView) findViewById(R.id.Date);
+
+        DName= findViewById(R.id.et_Device);
+        PName= findViewById(R.id.tv_Pracitioner);
+        Date= findViewById(R.id.Date);
+        tRequest=findViewById(R.id.tRequest);
+        tRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+        //Device name
+        DName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (DName.getText().length()<8)
+                {
+                    DName.setError("Please enter valid Device");
+                }
+            }
+        });
+
+        PName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (PName.getText().length()<8)
+                {
+                    PName.setError("Please enter valid Practitioner");
+                }
+            }
+        });
+        Date.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (Date.getText().length()<5)
+                {
+                    Date.setError("Please Select  a valid Date");
+                }
+            }
+        });
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,4 +106,11 @@ public class Request extends AppCompatActivity {
 
 
     }
+    public  void openDialog(){
+
+        requestDialog requestDialog= new  requestDialog();
+        requestDialog.show(getSupportFragmentManager(),"requestDialog");
+    }
+
+
 }
